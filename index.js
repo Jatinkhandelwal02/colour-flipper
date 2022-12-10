412b316e89b0c4d6f40c9d96c5f9a8b83f688417
Jatinkhandelwal02 <110752182+Jatinkhandelwal02@users.noreply.github.com>
2022-12-10 18:11:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+btn.addEventListener("click",random_bg_color);
+
+
+
+
+
+
+
+
+
+
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.backgroundColor = bgColor;
+    }
